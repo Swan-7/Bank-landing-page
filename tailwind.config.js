93c054691480cbbefd23d58f4b665dde9abd8515
Benita Daniel <benitadaniel.dev@gmail.com>
2023-02-19 14:24:25 +0100

    Project update

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      width: {
+        "wd-24": "24%"
+      }
+    },
+  },
+  plugins: [],
+}
